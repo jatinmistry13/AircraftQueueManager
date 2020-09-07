d838c8e4934d81bfc708b17994f0b9d45109f077
Jatin <jmistry@microstrategy.com>
2020-09-06 22:32:31 -0400

    adding REST api controllers, service and models

---
+++ src/main/java/com/application/rest/controllers/AircraftQueueController.java
@@ -0,0 +1,38 @@
+package com.application.rest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.application.entity.Aircraft;
+import com.application.rest.models.AircraftInfo;
+import com.application.rest.service.ApiService;
+
+@RestController
+@RequestMapping("/queues")
+public class AircraftQueueController {
+    
+    @Autowired
+    private ApiService apiService;
+    
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public ResponseEntity<List<Aircraft>> listAircrafts() {
+        return apiService.listAircrafts();
+    }
+    
+    @RequestMapping(value="/", method = RequestMethod.PUT)
+    public ResponseEntity<Object> addAircraftToQueue(@RequestBody AircraftInfo aircraftInfo) {
+        return apiService.addAircraftToQueue(aircraftInfo);
+    }
+    
+    @RequestMapping(value="/", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> removeAircraftFromQueue() {
+        return apiService.removeAircraftFromQueue();
+    }
+
+}
---
+++ src/main/java/com/application/rest/models/ResponseWrapper.java
@@ -0,0 +1,19 @@
+package com.application.rest.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ResponseWrapper {
+    private boolean success;
+    private String message;
+    private Object data;
+    private Object error;
+}
